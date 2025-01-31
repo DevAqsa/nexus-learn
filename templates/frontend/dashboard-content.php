ca8d72befa26b024c0df8f044562526e0d960a9b
Aqsa Mumtaz <codewithaqsa@gmail.com>
2025-01-31 15:47:39 +0500

    fix error

--- templates/frontend/dashboard-content.php
+++
@@ -1,4 +0,0 @@
-<div class="nl-dashboard-wrapper">
-    <!-- Your existing dashboard content from the previous template -->
-    <!-- This includes the user info, stats, courses grid, etc. -->
-</div>
